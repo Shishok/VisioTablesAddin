2dcae5afc8cc39ad440cde24fae07c1bba2f131a
Shishok <Shishok@users.noreply.github.com>
2023-03-17 14:13:48 +0300

    Add files via upload

--- VisioTablesAddin/My Project/AssemblyInfo.vb
+++ VisioTablesAddin/My Project/AssemblyInfo.vb
@@ -12,7 +12,7 @@ Imports System.Runtime.InteropServices
 <Assembly: AssemblyDescription("")> 
 <Assembly: AssemblyCompany("")> 
 <Assembly: AssemblyProduct("")> 
-<Assembly: AssemblyCopyright("Copyright ©  2014")> 
+<Assembly: AssemblyCopyright("Copyright ©  2023")> 
 <Assembly: AssemblyTrademark("")> 
 
 <Assembly: ComVisible(True)> 
